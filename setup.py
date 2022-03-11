f11dc6ed80c5a260b696d53788251af9a3db8cf3
Ricardo-Jackson-Ferrari <ricjferrari@gmail.com>
2022-03-11 17:28:10 -0300

    Fix bugs

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 
 def read(filename):
     return [req.strip() for req in open(filename).readlines()]
@@ -11,5 +12,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=read('requirements.txt'),
-    extras_require={'dev': read('requirements-dev.txt')}
+    extras_require={'dev': read('requirements-dev.txt')},
 )
