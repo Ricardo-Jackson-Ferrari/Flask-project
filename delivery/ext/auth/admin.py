5b00d8e95908eb501ab3e4e0a766c69262d8d6c2
Ricardo-Jackson-Ferrari <ricjferrari@gmail.com>
2022-03-16 00:56:43 -0300

    admin-user upgrade

--- delivery/ext/auth/admin.py
+++ delivery/ext/auth/admin.py
@@ -2,8 +2,11 @@ from flask_admin.contrib.sqla import ModelView
 
 
 def format_user(self, request, user, *args):
-    return user.email.upper()
+    # return user.email
+    return user.email.replace(user.email[user.email.rindex('@'):], '*'*len(user.email[user.email.rindex('@'):]))
 
 class UserAdmin(ModelView):
     """Admin interface"""
     column_formatters = {'email': format_user}
+    column_list = ('name', 'email', 'admin')
+    form_columns = ('name', 'email', 'password', 'admin')
